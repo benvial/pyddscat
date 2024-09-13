bb84e27c21063bd2f6a90459a25e2f66ef088e70
Benjamin Vial <benjamin.vial84@gmail.com>
2024-09-13 15:45:38 +0100

    packaging

--- pyddscat/utils.py
+++ pyddscat/utils.py
@@ -216,10 +216,13 @@ def resolve_profile(fname):
         else:
             full_name = None
     resources = importlib.resources.files("pyddscat")
-    # print(resources)
-    if full_name is None and os.path.join("profiles", fname) in resources:
+    print("resources", resources)
+    print("full_name", full_name)
+    if full_name is None:  # and os.path.join("profiles", fname) in resources:
         # return resource_filename("pyddscat", os.path.join("profiles", fname))
-        return importlib.resources.path("pyddscat", os.path.join("profiles", fname))
+        return importlib.resources.files("pyddscat").joinpath(
+            os.path.join("profiles", fname)
+        )
     return full_name
 
 
